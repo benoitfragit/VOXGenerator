b5d303e85fa06909595ec471ad27db099f205bf5
benoitfragit <benoitfranquet@gmail.com>
2015-06-03 21:33:22 +0200

    Adding package setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+    name = 'voxgenerator',
+    packages = ['voxgenerator'],
+    version = '1.0.0',
+    description = 'Vox generator',
+    author = 'Benoit Franquet',
+    author_email = 'benoitfranquet@gmail.com'
+)
