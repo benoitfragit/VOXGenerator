b1215afc6fd8e2943b1658e9adf426e48df00ee2
benoitfragit <benoitfranquet@gmail.com>
2015-06-25 22:02:47 +0200

    fixing id

--- voxgenerator/pipeline/selector.py
+++ voxgenerator/pipeline/selector.py
@@ -9,7 +9,7 @@ from voxgenerator.generator import Checksum
 from activation import *
 
 class Selector:
-    def __init__(self, xml):        
+    def __init__(self, xml):
         self.__priority__  = {}
         self.__plugins__   = {}
         self.__current_model_id__ = -1
@@ -17,11 +17,11 @@ class Selector:
         self.__default__ = ""
         self.__activations__ = {}
         self.__build__(xml)
-        
+
     def __getvalidpluginnames__(self, include):
         for f in include:
             plugin_xml = f.get("file")
-                        
+
             checker = Checksum()
             if checker.__haschanged__(plugin_xml):
                 plugin_generator = PluginGenerator(plugin_xml)
@@ -30,37 +30,39 @@ class Selector:
             if os.path.isfile(plugin_xml):
                 plugin_tree = etree.parse(plugin_xml)
                 plugins = plugin_tree.xpath("/plugins/plugin")
-            
+
+                plugin_id = 0
                 for plugin in plugins:
                     if not self.__plugins__.has_key(plugin.get("name")):
-                        self.__plugins__[plugin.get("name")] = plugin.get("id")
-                        
+                        self.__plugins__[plugin.get("name")] = plugin_id
+                        plugin_id += 1
+
     def __build__(self, xml):
         if os.path.isfile(xml):
             pipeline_tree = etree.parse(xml)
 
             root      = pipeline_tree.xpath("/pipelines")
-            pipelines = root[0].findall("pipeline") 
+            pipelines = root[0].findall("pipeline")
             includes  = root[0].findall("include")
             lmctl     = root[0].find("lmctl")
             self.__lmctl__ = lmctl.get("file")
-            
+
             self.__getvalidpluginnames__(includes)
             self.__parsepipelines__(pipelines)
-            
+
     def __parsepipelines__(self, pipelines):
         for pipeline in pipelines:
             plugin   = pipeline.get("plugin")
             priority = pipeline.get("priority")
             default  = pipeline.get("default")
-        
+
             if self.__plugins__.has_key(plugin):
                 id = self.__plugins__[plugin]
                 self.__priority__[id] = priority
                 self.__activations__[id] = []
 
                 self.__parseactivation__(id, pipeline)
-                
+
                 if default == "True":
                     self.__current_model_id__ = id
                     self.__default__ = plugin
@@ -69,20 +71,20 @@ class Selector:
 
     def __parseactivation__(self, id, pipeline):
         activations = pipeline.findall("activation")
-        
-        for active in activations:            
-            window = active.get("window") 
+
+        for active in activations:
+            window = active.get("window")
             if window is not None:
                 self.__activations__[id].append(WindowActivation(window))
-            
+
             if eval(active.get("mouse")) == True:
                 x = active.find("x")
                 y = active.find("y")
-                self.__activations__[id].append(MouseActivation([int(x.get("start")), 
+                self.__activations__[id].append(MouseActivation([int(x.get("start")),
                                                                  int(x.get("end")),
                                                                  int(y.get("start")),
                                                                  int(y.get("end"))]))
-    
+
             keyword = active.get("keyword")
             if keyword is not None:
                 self.__activations__[id].append(KeywordActivation(keyword))
@@ -93,9 +95,9 @@ class Selector:
                 field = [eval(elm) for elm in t]
                 self.__activations__[id].append(TimeActivation(field))
 
-    def __updateactivations__(self, hyp):        
-        sorted_priority = list(reversed(sorted(self.__priority__, key=self.__priority__.__getitem__))) 
-        
+    def __updateactivations__(self, hyp):
+        sorted_priority = list(reversed(sorted(self.__priority__, key=self.__priority__.__getitem__)))
+
         self.__current_model_id__ = -1
         for id in sorted_priority:
             if len(self.__activations__[id]) == 0:
@@ -106,14 +108,14 @@ class Selector:
                 if activation.__isactive__(hyp):
                     self.__current_model_id__ = id
                     return
-                
+
     def __getactivatedlm__(self, hyp):
         self.__updateactivations__(hyp)
-        
+
         for lm in self.__plugins__:
             if self.__plugins__[lm] == self.__current_model_id__:
                 return lm
-        
+
         return self.__default__
 
     def __getdefault__(self):
